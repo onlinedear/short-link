3573a475f6919572ebf9b6743c4cc15ddce5c1dd
xiaoxin <onlinedear@163.com>
2024-11-16 12:41:44 +0800

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -42,7 +42,7 @@ const Navbar = () => {
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <Link href="/" className="flex items-center gap-3">
               <Image src="/box.svg" alt="logo" width={30} height={30} />
-              <div className="font-bold text-lg">S-Link</div>
+              <div className="font-bold text-lg">Haoduan🤏</div>
             </Link>
             <div className="md:hidden">
               <button
@@ -98,14 +98,7 @@ const Navbar = () => {
                   </li>
                 );
               })} */}
-              <li>
-                <NavLink
-                  href="https://github.com/Zhang-BigSmart/short-link"
-                  className="block font-medium text-sm text-white bg-gray-800 hover:bg-gray-600 active:bg-gray-900 md:inline"
-                >
-                  Github
-                </NavLink>
-              </li>
+
             </ul>
           </div>
         </div>
